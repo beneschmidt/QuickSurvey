095a2ced493f84d47d5f71a9e305eeda731c31c9
arthurliebhardt <liebhardt.arthur@gmail.com>
2015-05-02 23:28:59 +0200

    [FEATURE] changed footer content from the side bar

--- view/Menu.view.js
+++ view/Menu.view.js
@@ -38,8 +38,8 @@ sap.ui.jsview("ui5bp.view.Menu", {
             content: [oList],
             footer: new sap.m.Bar({
                 contentMiddle: [new sap.m.Link("myproLink", {
-                    text: "v0.8.0",
-                    href: "http://blog.mypro.de/tag/ui5boilerplate/"
+                    text: "v0.1.0",
+                    href: "https://github.com/beneschmidt/QuickSurvey"
                 })]
             })
         });
