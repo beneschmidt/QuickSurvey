226126ee8fc65e0239dc728f57b3c1922c8b02ff
Benne <benediktschmidt89@gmail.com>
2015-08-27 10:02:54 +0200

    Database stuff added, working for the full survey currently. Probably need to include a database backup file

---
+++ server/dbutils.js
@@ -0,0 +1,39 @@
+module.exports = {
+  extractFullSurveyList : function(rows, log){
+    var sid, qid;
+    var objects = [];
+    var spos = -1, qpos = -1;
+    for(var i = 0; i <rows.length; i++){
+      var current = rows[i];
+      if(sid!=current.sid){
+        sid=current.sid;
+        spos++;
+        qpos = -1;
+        objects[spos] = {
+          objectId : current.sid,
+          name : current.name,
+          questions:[]
+        }
+      } else if(qid!= current.qid){
+        qpos++;
+        qid = current.qid
+        objects[spos].questions.push({
+          objectId: current.qid,
+          questionText: current.questiontext,
+          multiple: current.multiple,
+          answers: []
+        });
+      } else {
+        objects[spos].questions[qpos].answers.push({
+          objectId: current.aid,
+          text : current.text
+        });
+      }
+    }
+    log.info(objects);
+    var fullList = {
+      Survey: objects
+    }
+    return fullList;
+  }
+}
--- server/rest.js
+++ server/rest.js
@@ -5,7 +5,7 @@ module.exports = {
 	http : null,
 	fs : null,
 	first: true,
-	
+
 	getSurveyList : function(res, req, http, log){
 		this.log = log;
 		this.http = http;
@@ -15,31 +15,16 @@ module.exports = {
 		this.first = !this.first;
 		var fileId = this.first?1:2;
 		var fileName = 'model/survey'+fileId+'.json';
-		
-		this.fs.readFile(fileName, 'utf8', function (err,data) {
-			if (err) {
-				return console.log(err);
-			}
-			var json = JSON.parse(data);
-			res.setHeader("Access-Control-Allow-Origin", "http://localhost:8877");
-			log.info("write JSON: "+JSON.stringify(json));
-			res.writeHead(200, {'Content-Type': 'text/plain'});
-			res.write(JSON.stringify(json) + "\n");
-			res.end();
-		});
-		
-		
-	},
-	
-	/*
-	returnSQLResult : function(res, req, http, log, table, constraint){
-		this.log = log;
-		this.http = http;
-		var that = this;
-		this.result = [];
-		var conString = "pg://thresh:thresh@localhost:5432/threshDB";
-		var pg = require("pg");
+
+		var http = require('http');
+		var pg = require('pg');
+
+		var conString = "postgres://quicksurvey:quicksurvey@localhost:5432/quicksurvey";
 		var oClient = new pg.Client(conString);
+		var table = "view_full_surveys";
+		var constraint;
+		this.result = [];
+		var dbutils = require("./dbutils.js");
 
 		oClient.connect(function(err) {
 			if(err) {
@@ -54,29 +39,41 @@ module.exports = {
 				if(constraint){
 					select += ' ' +constraint;
 				}
+				that.log.info(select);
 				var selectQuery = oClient.query(select);
 				selectQuery.on('row', function(row) {
 					that.result.push(row);
 				});
 				selectQuery.on('end', function(result) {
 					if(result.rowCount===0){
-						that.log.info("no "+table+" received!")	
-						oClient.end();				
+						that.log.info("no "+table+" received!");
+						that.log.info(result);
+						oClient.end();
 					} else {
-					    res.setHeader("Access-Control-Allow-Origin", "http://localhost:9001");
+						var toSend = dbutils.extractFullSurveyList(that.result, that.log);
+						that.log.info(toSend);
+
+						res.setHeader("Access-Control-Allow-Origin", "http://localhost:8877");
 						res.writeHead(200, {'Content-Type': 'text/plain'});
-						res.write(JSON.stringify(that.result) + "\n");
+						res.write(JSON.stringify(toSend) + "\n");
 						res.end();
 						oClient.end();
 					}
-				})
-				selectQuery.on('error', function(err) {
-					that.log.error(err + " cannot receive "+table);
-					oClient.query('ROLLBACK', function() {
-						oClient.end();
-					});
 				});
 			});
 		});
-	}*/
+	},
+
+	/*this.fs.readFile(fileName, 'utf8', function (err,data) {
+	if (err) {
+	return console.log(err);
+}
+var json = JSON.parse(data);
+res.setHeader("Access-Control-Allow-Origin", "http://localhost:8877");
+log.info("write JSON: "+JSON.stringify(json));
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.write(JSON.stringify(json) + "\n");
+res.end();
+});*/
+
 }
